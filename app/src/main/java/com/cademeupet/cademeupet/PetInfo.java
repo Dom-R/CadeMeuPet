6d960c497946edae01ff2b63cf8336e945cc22d7
Dom <dominik.reller@gmail.com>
2016-10-13 11:51:40 -0300

    Adicionado getter de sexo do animal.

--- app/src/main/java/com/cademeupet/cademeupet/PetInfo.java
+++ app/src/main/java/com/cademeupet/cademeupet/PetInfo.java
@@ -13,6 +13,10 @@ public class PetInfo {
         return name;
     }
 
+    public String getSex() {
+        return sex;
+    }
+
     public PetInfo(String name, String sex) {
         this.name = name;
         this.sex = sex;
