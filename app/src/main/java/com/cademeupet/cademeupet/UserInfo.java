7070861f99a49ad86e3e49ce432c1140c08fd677
Dom <dominik.reller@gmail.com>
2016-10-12 23:16:11 -0300

    Adicionado variavel para guardar numero do telefone e alguns metodos que virao a ser usados na classe do Usuário.

--- app/src/main/java/com/cademeupet/cademeupet/UserInfo.java
+++ app/src/main/java/com/cademeupet/cademeupet/UserInfo.java
@@ -1,17 +1,36 @@
 package com.cademeupet.cademeupet;
 
+import java.util.ArrayList;
+
 public class UserInfo {
 
     public String name;
     public String email;
+    public String phoneNumber;
 
     public UserInfo() {
         // Default constructor required for calls to DataSnapshot.getValue(User.class)
+
     }
 
-    public UserInfo(String name, String email) {
+    public String getName() {
+        return name;
+    }
+
+    /*
+    public void addPet(String id) {
+        petsID.add(id);
+    }
+
+    public ArrayList<String> getPetList() {
+        return petsID;
+    }*/
+
+    public UserInfo(String name, String email, String phoneNumber) {
         this.name = name;
         this.email = email;
+        this.phoneNumber = phoneNumber;
+        //this.petsID = new ArrayList<String>();
     }
 
 }
