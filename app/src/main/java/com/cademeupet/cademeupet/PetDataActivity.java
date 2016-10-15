84f79f4ab671a459210f77f24312de465eaaccfc
Dom <dominik.reller@gmail.com>
2016-10-15 01:19:59 -0300

    Implementado detectação de localização no app através do GPS.

--- app/src/main/java/com/cademeupet/cademeupet/PetDataActivity.java
+++ app/src/main/java/com/cademeupet/cademeupet/PetDataActivity.java
@@ -1,10 +1,17 @@
 package com.cademeupet.cademeupet;
 
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AlertDialog;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Looper;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.ViewGroup;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -15,6 +22,12 @@ import com.google.firebase.database.ValueEventListener;
 
 public class PetDataActivity extends AppCompatActivity {
 
+    private static final int REQUEST_GPS = 8572;
+    private LocationManager locationManager;
+    private Criteria criteria;
+    private Looper looper;
+    private LocationListener locationListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,8 +35,6 @@ public class PetDataActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         final String petID = intent.getStringExtra("PET_DATA");
-
-        final PetDataActivity currentClass = this;
 
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference ref = database.getReference("pets/" + petID);
@@ -46,10 +57,123 @@ public class PetDataActivity extends AppCompatActivity {
             }
         });
 
-        // Insere no Banco de Dados o acesso ao ID de um cachorro
-        //FirebaseDatabase database = FirebaseDatabase.getInstance();
-        //DatabaseReference myRef = database.getReference("message");
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(final Location location) {
+                Log.d("Location Changes", location.toString());
+                System.out.println("GPS Latitude: " + String.valueOf(location.getLatitude()));
+                System.out.println("GPS Longitude: " + String.valueOf(location.getLongitude()));
 
-        //myRef.setValue("Ped Id: " + petId);
+                // Insere no Banco de Dados o acesso ao ID de um cachorro
+                final FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference ref = database.getReference("pets/" + petID);
+
+                ref.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        //String name = (String) dataSnapshot.getValue();
+                        //System.out.println(name);
+
+                        PetInfo pet = dataSnapshot.getValue(PetInfo.class);
+
+                        pet.setLastLocation(location.getLatitude() + "," + location.getLongitude());
+
+                        DatabaseReference reference = database.getReference("pets/" + petID);
+                        reference.setValue(pet);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        System.out.println("The read failed: " + databaseError.getCode());
+                    }
+                });
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+                Log.d("Status Changed", String.valueOf(status));
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+                Log.d("Provider Enabled", provider);
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Log.d("Provider Disabled", provider);
+            }
+        };
+
+        // Now create a location manager
+        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+
+        // Now first make a criteria with your requirements
+        // this is done to save the battery life of the device
+        // there are various other other criteria you can search for..
+        criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+        criteria.setPowerRequirement(Criteria.POWER_LOW);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+        criteria.setSpeedRequired(false);
+        criteria.setCostAllowed(true);
+        criteria.setHorizontalAccuracy(Criteria.ACCURACY_HIGH);
+        criteria.setVerticalAccuracy(Criteria.ACCURACY_HIGH);
+
+        // This is the Best And IMPORTANT part
+        looper = null;
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_GPS);
+            return;
+        }
+        locationManager.requestSingleUpdate(criteria, locationListener, looper);
+        //System.out.println(location.get("longitude") + " " + location.get("latitude"));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_GPS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        ActivityCompat.requestPermissions(this,
+                                new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                                REQUEST_GPS);
+                        return;
+                    }
+                    locationManager.requestSingleUpdate(criteria, locationListener, looper);
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 }
--- app/src/main/java/com/cademeupet/cademeupet/PetInfo.java
+++ app/src/main/java/com/cademeupet/cademeupet/PetInfo.java
@@ -1,9 +1,12 @@
 package com.cademeupet.cademeupet;
 
+import android.location.Location;
+
 public class PetInfo {
 
     public String name;
     public String sex;
+    public String lastLocation;
 
     public PetInfo() {
         // Default constructor required for calls to DataSnapshot.getValue(User.class)
@@ -17,9 +20,18 @@ public class PetInfo {
         return sex;
     }
 
+    public String getLastLocation() {
+        return lastLocation;
+    }
+
+    public void setLastLocation(String lastLocation) {
+        this.lastLocation = lastLocation;
+    }
+
     public PetInfo(String name, String sex) {
         this.name = name;
         this.sex = sex;
+        lastLocation = ",";
     }
 
 }
