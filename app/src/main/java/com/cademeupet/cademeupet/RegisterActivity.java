990a2c6e3d89229c40d8cb4ee2e0baf2aaff767d
Dom <dominik.reller@gmail.com>
2016-10-15 21:59:36 -0300

    Adicionado \Validação do telefone

--- app/src/main/java/com/cademeupet/cademeupet/RegisterActivity.java
+++ app/src/main/java/com/cademeupet/cademeupet/RegisterActivity.java
@@ -10,6 +10,8 @@ import android.widget.EditText;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.regex.Pattern;
+
 public class RegisterActivity extends AppCompatActivity {
 
     private DatabaseReference mDatabase;
@@ -45,8 +47,8 @@ public class RegisterActivity extends AppCompatActivity {
             inputFullname.setError("Full name is required");
         } else if (!isValidEmail(inputEmail.getText().toString())) {
             inputEmail.setError("Invalid Email");
-        } else if (inputNumber.getText().toString().trim().equals("")) {
-            inputFullname.setError("Phone number is required");
+        } else if (inputNumber.getText().toString().trim().equals("") || !android.util.Patterns.PHONE.matcher(inputNumber.getText().toString().trim()).matches() || !Pattern.matches("^[+]?[0-9]{8,14}$", inputNumber.getText().toString().trim())) {
+            inputNumber.setError("Phone number is required");
         } else {
             UserInfo user = new UserInfo(inputFullname.getText().toString(), inputEmail.getText().toString(), inputNumber.getText().toString());
 
